8d2b68276641fa7160914bdd3c47b0459d7323fc
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2022-10-19 13:08:44 -0400

    video to 2:56

---
+++ index.js
@@ -0,0 +1,7 @@
+const chalk = require('chalk');
+const log = console.log;
+
+// Basic colors
+log(chalk.red('Hey this is dcode!'));
+log(chalk.magenta('I am magenta'));
+log(chalk.yellow('Banana.'));
