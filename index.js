e6070ca32f9cf5129a8c591e915d48e9122b553a
ryan-rutishauser <98336166+ryan-rutishauser@users.noreply.github.com>
2022-10-23 18:17:44 -0400

    emmitter classes working

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+/* eslint-disable prefer-destructuring */
 const chalk = require('chalk');
+
 const log = console.log;
 
 // Basic colors
 log(chalk.red('Hey this is dcode!'));
 log(chalk.magenta('I am magenta'));
 log(chalk.yellow('Banana.'));
+
+log(chalk.bgRed('Yeah I have a red background'));
+log(chalk.bgGreen('Forest'));
